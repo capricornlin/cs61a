fca043482e14579ac693e3471b5584bc189c5d8f
林 <casey@linde-MacBook-Air.local>
2018-07-17 00:19:19 +0800

    mutabble

--- SICP/2.3/linkedlist.py
+++ SICP/2.3/linkedlist.py
@@ -40,5 +40,148 @@ def getitem_link(s, i):
 	return first(s)
 	
 
+def len_link_recursive(s):
+	if s == empty:
+		return 0
+	return 1 + len_link_recursive(rest(s))
+
+def getitem_link_recursive(s, i):
+	if i == 0:
+		return first(s)
+	return getitem_link_recursive(rest(s),i - 1)
+
+""" 1. simple way to try recursion len_link_recursive and getitem_link_recursive 
+	2. Because rst(s) return s[1], the last sequence's element(s[1]) will be 'empty'.
+	3. So we can get the length. 
+"""
+empty = 'empty'
+
+def first(s):
+       return s[0]
+def rest(s):
+       return s[1]
+       
+
+   
+four = [1, [2, [3, [4, 'empty']]]]
+
+def len_link_recursive(s):
+    if s == empty:
+        return 0
+    return 1 + len_link_recursive(rest(s))
+
+def getitem_link_recursive(s, i):
+    if i == 0:
+        return first(s)
+    return getitem_link_recursive(rest(s),i - 1)
+    
+    
+len_link_recursive(four)
+
+
+""" # """
+def extend_link(s,t):
+	assert is_link(s) and is_link(t)
+	if s == empty:
+		return t
+	else:
+		return link(first(s),extend_link(rest(s),t))
+
+
+""" # """
+def apply_to_all_link(f, s):
+	assert is_link(s)
+	if s == empty:
+		return s
+	else:
+		return link(f(first(s)),apply_to_all_link(f,rest(s)))
+
+""" # """
+def keep_if_link(f,s):
+	assert is_link(s)
+	if s == empty:
+		return s
+	else:
+		kept = keep_if_link(f, rest(s))
+		if f(first(s)):
+			return link(first(s), kept)
+		else:
+			return kept
+
+""" # """
+def join_link(s, separator):
+	if s == empty:
+		return ""
+	elif rest(s) == empty:
+		return str(first(s))
+	else:
+		return str(first(s)) + separator + join_link(rest(s), separator)
+
+""" # """
+def partitions(n,m):
+
+	if n == 0:
+		return link(empty, empty)
+	elif n < 0 or m == 0:
+		return empty
+	else:
+		using_m = partitions(n-m, m)
+		with_m = apply_to_all_link(lambda s: link(m, s), using_m)
+		without_m = partitions(n, m-1)
+		return extend_link(with_m, without_m)
+
+""" # """
+def print_partitions(n, m):
+    lists = partitions(n, m)
+    strings = apply_to_all_link(lambda s: join_link(s, " + "), lists)
+    print(join_link(strings, "\n"))
+
+
+
+
+
+
+
+
+
+
+
+
+		
+
+			
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
---
+++ SICP/2.4/MutableData.py
@@ -0,0 +1,34 @@
+
+
+
+def make_withdraw(balance):
+        """Return a withdraw function that draws down balance with each call."""
+        def withdraw(amount):
+            nonlocal balance                 # Declare the name "balance" nonlocal
+            if amount > balance:
+                return 'Insufficient funds'
+            balance = balance - amount       # Re-bind the existing balance name
+            return balance
+        return withdraw
+
+wd = make_withdraw(100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
